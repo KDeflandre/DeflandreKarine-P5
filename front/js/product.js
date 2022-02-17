11acb6d4ebadda2d6a25c5ca19d20eaf43bc41c1
Karine <kadeflandre@gmail.com>
2022-02-18 00:05:03 +0100

    correction script + start product

---
+++ front/js/product.js
@@ -0,0 +1,5 @@
+let params = (new URL(window.location.href)).searchParams;
+let idProduct = params.get("id");
+console.log(idProduct);
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,8 @@
 
- allProducts (); 
-
- NewProducts ();
+ NewProducts();
 
   // Test V2 recup produits
-  async function allProducts () {
+  async function allProducts() {
   try {
     let responseProducts = await fetch('http://localhost:3000/api/products/');
       console.log("Products are ok =)")
@@ -15,45 +13,63 @@
     return []
       }
     }
-// Création et ajout des produits dans le DOM 
 
-  async function NewProducts () {
-    let result = await allProducts ()
-    .then( (responseProducts) => {
-      for (let i=0; i < responseProducts.length; i++) {		
+// // Création et ajout des produits dans le DOM 
 
-       // ajout a 
-        let productLink = document.createElement("a");
-        document.querySelector(".items").appendChild(productLink);
-        productLink.href = `product.htlm?id=${responseProducts[i]._id}`;
+//   async function NewProducts() {
+//     let result = await allProducts()
+//     .then( (responseProducts) => {
+//       for (let i=0; i < responseProducts.length; i++) {		
 
-        // ajout article
-        let productArticle = document.createElement("article");
-        productLink.appendChild(productArticle);
+//        // ajout a 
+//         let productLink = document.createElement("a");
+//         document.querySelector(".items").appendChild(productLink);
+//         productLink.href = `product.html?id=${responseProducts[i]._id}`;
 
-        // ajout image du produit
-        let productImg = document.createElement("img");
-        productArticle.appendChild(productImg);
-        productImg.src = responseProducts[i].imageUrl;
-        productImg.alt = responseProducts[i].imageUrl;
+//         // ajout article
+//         let productArticle = document.createElement("article");
+//         productLink.appendChild(productArticle);
 
-        // ajout du titre du produit avec H3 
-        let productName = document.createElement("h3");
-        productArticle.appendChild(productName);
-        productName.classList.add("productName");
-        productName.innerHTML = responseProducts[i].name;
+//         // ajout image du produit
+//         let productImg = document.createElement("img");
+//         productArticle.appendChild(productImg);
+//         productImg.src = responseProducts[i].imageUrl;
+//         productImg.alt = responseProducts[i].imageUrl;
 
-        // ajout de la description avec p 
-        let productDescription = document.createElement("p");
-        productArticle.appendChild(productDescription);
-        productDescription.classList.add("productDescription");
-        productDescription.innerHTML = responseProducts[i].description;
+//         // ajout du titre du produit avec H3 
+//         let productName = document.createElement("h3");
+//         productArticle.appendChild(productName);
+//         productName.classList.add("productName");
+//         productName.innerHTML = responseProducts[i].name;
 
-    }
-});
-console.log("Product are integrated");
-}
+//         // ajout de la description avec p 
+//         let productDescription = document.createElement("p");
+//         productArticle.appendChild(productDescription);
+//         productDescription.classList.add("productDescription");
+//         productDescription.innerHTML = responseProducts[i].description;
+
+//     }
+//     console.log("Product are integrated");
+// });
+
+// }
     
 
-
+async function NewProducts() {
+  await allProducts()
+    .then((responseProducts) => {
+      const productContainer = document.getElementById("items")
+      responseProducts.forEach(product => {
+        productContainer.insertAdjacentHTML('beforeend', 
+        `<a href="product.html?id=${product._id}">
+        <article>
+        <img src="${product.imageUrl}" alt="${product.name}">
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+        </article>
+        </a>`);
+      });
+    });
+  console.log("Product are integrated");
+}
 
